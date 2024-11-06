9749dc9803c487bae49d51019b9548cf4f43069b
m1chae11u <michael.lu@berkeley.edu>
2024-11-06 04:01:14 +0000

    update

--- env_setup.sh
+++ env_setup.sh
@@ -17,22 +17,10 @@ fi
 
 conda activate pokerenv
 
-function install_if_missing {
-    package=$1
-    if ! conda list | grep -qw "$package"; then
-        echo "Installing $package..."
-        shift
-        conda install "$@" -y
-    else
-        echo "$package is already installed."
-    fi
-}
-
-# Check and install required packages
-install_if_missing "pytorch" "pytorch torchvision torchaudio pytorch-cuda=12.2 -c pytorch -c nvidia"
-install_if_missing "cudatoolkit-dev" "-c nvidia cudatoolkit-dev=12.2"
-install_if_missing "cudnn" "-c conda-forge cudnn=8.1"
-install_if_missing "transformers" "transformers pandas numpy=1.26.4 tqdm scikit-learn"
+# Install packages if not already installed
+echo "Installing packages..."
+conda install pytorch torchvision torchaudio -c pytorch -c nvidia -y
+conda install transformers pandas numpy=1.26.4 tqdm scikit-learn -y
 
 # Install additional Python packages from requirements.txt
 if [ -f "$SCRIPT_DIR/requirements.txt" ]; then
